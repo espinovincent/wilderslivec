136ca9c196ae5fa254de051b2e77a5ab8e87a24e
espino vincent <tantalas95@gmail.com>
2024-01-04 11:09:55 +0100

    fin exo

---
+++ npm-livecoding/index.js
@@ -0,0 +1,7 @@
+import chalk from 'chalk';
+
+console.log('Hello');
+console.log(chalk.blue('kanna'));
+console.log(chalk.red('samuel'));
+console.log(chalk.green('nassim'));
+console.log(chalk.yellowBright('ayouba'));
